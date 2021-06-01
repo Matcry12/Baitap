d2ef63f59dfdbec1fdb5760d6ae3d6ebefa84b10
Matcry12 <fdtywadw@gmail.com>
2021-06-01 14:55:44 +0700

    hiển thị màn hình

--- app/src/main/java/com/szxc/myapplication123/MainActivity.kt
+++ app/src/main/java/com/szxc/myapplication123/MainActivity.kt
@@ -4,11 +4,13 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Toast.makeText(this, "tứng tưng từng tưng", Toast.LENGTH_SHORT).show()
         }
 
 }
