61fc9e3782cffeec91e63cbbbafabc49ed875730
Matcry12 <fdtywadw@gmail.com>
2021-06-03 22:04:48 +0700

    bai tap 1/6

--- app/src/main/java/com/szxc/myapplication123/MainActivity.kt
+++ app/src/main/java/com/szxc/myapplication123/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +12,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Toast.makeText(this, "tứng tưng từng tưng", Toast.LENGTH_SHORT).show()
+        val editText = findViewById<EditText>(R.id.nhapten)
+        val button = findViewById<Button>(R.id.bac)
+        button.setOnClickListener(){
+          val intent : Intent = Intent(this, MainActivity2::class.java)
+            val name = editText.text.toString()
+            Toast.makeText(this, name, Toast.LENGTH_SHORT).show()
+            intent.putExtra("dare", name)
+            startActivity(intent)
+        }
         }
 
 }
